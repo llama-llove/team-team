df1c9530b325a91836dcffc9b96f0f68db460a91
llama-llove <65976596+llama-llove@users.noreply.github.com>
2020-08-19 04:03:40 -0700

    Added movement in arms and body

---
+++ Game/Assets/mouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class mouse : MonoBehaviour
+{
+    public Vector3 mouseposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseposition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        transform.position = Camera.main.ScreenToWorldPoint(mouseposition);
+    }
+}
---
+++ Game/Assets/movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody2D self;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            self.MovePosition(new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y));
+        }
+        if (Input.GetKey("d"))
+        {
+            self.MovePosition(new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y));
+        }
+    }
+}
