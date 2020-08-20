90316d1ed2d8cc15cc8250bcecd2875aff7f5809
llama-llove <65976596+llama-llove@users.noreply.github.com>
2020-08-20 02:00:52 -0700

    changed button

---
+++ Game/Assets/button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    public int speed;
+    public Rigidbody2D self;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("die", 1);
+            while (true)
+            {
+                self.MovePosition(new Vector2(self.position.x+speed*Time.deltaTime,self.position.y));
+            }
+        }
+    }
+    private void die()
+    {
+        Destroy(self.gameObject);
+    }
+
+}
