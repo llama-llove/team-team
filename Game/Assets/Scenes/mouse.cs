0b5095398b946dcacb21a9cbe74306f1f175aad6
llama-llove <65976596+llama-llove@users.noreply.github.com>
2020-08-20 23:24:09 -0700

    idk what i changed

--- Game/Assets/Scenes/mouse.cs
+++ Game/Assets/Scenes/mouse.cs
@@ -7,7 +7,6 @@ public class mouse : MonoBehaviour
 {
     public Vector3 mouseposition;
     public int speed;
-    //public List<Rigidbody2D> armparts;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,37 +23,8 @@ public class mouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0), Space.World);
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0), Space.World);
-        //}
-        //if (Input.GetKey(KeyCode.UpArrow))
-        //{
-        //    transform.Translate(new Vector3(0, speed * Time.deltaTime, 0), Space.World);
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow))
-        //{
-        //    transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0), Space.World);
-        //}
+
         mouseposition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        //if (mousedown())
-       // {
-        //    foreach (Rigidbody2D part in armparts)
-        //    {
-        //        part.isKinematic = true;
-        //    }
             transform.position = Camera.main.ScreenToWorldPoint(mouseposition);
-        //}
-        //else
-        //{
-        //    foreach (Rigidbody2D part in armparts)
-        //    {
-        //        part.isKinematic = false;
-        //    }
-        //}
     }
 }
---
+++ Game/Assets/hold.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class hold : MonoBehaviour
+{
+    public Rigidbody2D self;
+    public GameObject item;
+    public bool isholding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = gameObject.GetComponent<Rigidbody2D>();
+        isholding = false;
+    }
+    public bool mousedown()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            return true;
+        }
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (mousedown())
+        {
+            isholding = true;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, 1f);
+            item = hit.collider.gameObject;
+            item.transform.SetParent(transform, true);
+            item.GetComponent<Rigidbody2D>().isKinematic = true;
+            Debug.Log(item.name);
+        }
+        if(isholding&&!mousedown())
+        {
+            item.transform.SetParent(null,true);
+            item.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+    void objecthold()
+    {
+        
+        while (Input.GetMouseButton(0))
+        {
+
+        }
+
+
+    }
+}
